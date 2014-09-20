c1aa2d0f2f5c6a73eff026e269af2b0b76a96125
Masataka Yakura <masataka.yakura@gmail.com>
2014-09-20 21:55:29 +0900

    add fairly basic preview support

--- lycoris_preview.js
+++ lycoris_preview.js
@@ -1,1 +1,41 @@
 'use strict'
+
+//set up dialog
+var dialog = document.createElement('dialog')
+dialog.classList.add('lycoris-dialog')
+dialog.innerHTML = '<img><br><button>Close</button>'
+dialog.querySelector('button').onclick = function (e) {
+  e.preventDefault()
+  dialog.close()
+}
+dialog.oncancel = function () {
+  dialog.close()
+}
+dialog.onclose = function () {
+  var dialogImage = dialog.querySelector('img')
+  dialogImage.src = ''
+  dialogImage.alt = ''
+}
+document.body.appendChild(dialog)
+
+var attachments = query('.attachments > table')
+var imageAttachments = attachments.filter(function (attachment) {
+  // might be able to support other previewable files (SVG, BMP, etc.)
+  var images = ['gif', 'jpg', 'jpeg', 'png']
+  var name = attachment.querySelector('b').textContent.trim()
+  return images.indexOf(/.*\.(.*)$/.exec(name)[1]) > -1
+})
+
+imageAttachments.forEach(function (attachment) {
+  var name = attachment.querySelector('b').textContent.trim()
+  var url = new URL(attachment.querySelector('a[target]').href)
+
+  var a = attachment.querySelector('[colspan="3"] > a')
+  a.addEventListener('click', function (e) {
+    e.preventDefault()
+    var dialogImage = dialog.querySelector('img')
+    dialogImage.src = a.href
+    dialogImage.alt = name
+    dialog.showModal()
+  })
+})
